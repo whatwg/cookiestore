b9d9159525a63af56d82a4a3e7f540462a8878e4
Raymond Toy <rtoy@chromium.org>
2018-07-10 09:20:57 -0700

    Let compile.sh copy everything to the out directory
    Make compile.sh do the necessary copying to the out directory so that
    deploy.sh doesn't have to worry about it.  compile.sh is responsible
    for getting everything set up for the HTML version of spec.

--- compile.sh
+++ compile.sh
@@ -5,3 +5,7 @@ set -x
 
 # Run bikeshed.
 bikeshed --print=plain -f spec
+
+# The out directory should contain everything needed to produce the
+# HTML version of the spec.
+cp index.html out
--- deploy.sh
+++ deploy.sh
@@ -43,10 +43,6 @@ echo PWD = $PWD
 # Run our compile script; quit if it fails.
 doCompile || exit 1
 
-# Copy the things we need to the out directory (creating out/images if
-# needed).
-cp index.html out
-
 # Now let's go have some fun with the cloned repo
 cd out
 git config user.name "Travis CI"
